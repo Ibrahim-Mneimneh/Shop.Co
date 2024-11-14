76cebf2178928bb15f11530f06f8dab6d862b035
Ibrahim <ibrahim.mneimneh0@gmail.com>
2024-11-14 01:12:01 +0100

    fix CartModel.updatePrice

--- backend/src/models/cartModel.ts
+++ backend/src/models/cartModel.ts
@@ -2,11 +2,14 @@ import mongoose, {Document, Schema} from "mongoose"
 
 export interface ICart extends Document {
     user:mongoose.Schema.Types.ObjectId,
-    products:{
-        productId:mongoose.Schema.Types.ObjectId,
-        quantity:number
-    }[],
+    products:IProductRef[],
     totalPrice: Schema.Types.Decimal128
+    updatePrice(): Promise<void>;
+}
+export interface IProductRef {
+    productId: mongoose.Schema.Types.ObjectId;  
+    quantity: number;
+    price:Schema.Types.Decimal128            
 }
 
 const cartSchema = new Schema<ICart>({
@@ -14,21 +17,36 @@ const cartSchema = new Schema<ICart>({
     user:{type: mongoose.Schema.Types.ObjectId, ref: 'User',required:true},
     products:[{productId:{type:mongoose.Schema.Types.ObjectId,ref:"Product",required:true},quantity:{type: Number, 
       required: true, 
-      default: 0 }
+      default: 0 },
+    price:{ type: Schema.Types.Decimal128,  required: true }
     }],
     totalPrice:{type:Schema.Types.Decimal128,default: 0}
 });
 
-cartSchema.pre("save",async function(next){
-    let total=0;
-    for (let product of this.products){
-        const productData= await mongoose //Continue it after writing the Product Model
-        if(productData){
-            total+=product.quantity*parseFloat(productData.price.toString()); // ProductModel ref
+cartSchema.methods.updatePrice = async function (): Promise<void> {
+    let total = 0;
+
+    for (let product of this.products) {
+        const productData = await mongoose.model('Product').findById(product.productId);
+        if (productData) {
+            total += product.quantity * parseFloat(productData.price.toString());
         }
     }
-    this.totalPrice=mongoose.Types.Decimal128.fromString(total.toString())
+
+    this.totalPrice = mongoose.Types.Decimal128.fromString(total.toString());
+};
+
+cartSchema.pre("save",async function(next){
+    try{
+        await this.updatePrice()
+        next()
+    }
+    catch(error:any){
+        next(error)
+    }
 })
+
+
 
 
 export const CartModel =mongoose.model<ICart>("Cart",cartSchema);
