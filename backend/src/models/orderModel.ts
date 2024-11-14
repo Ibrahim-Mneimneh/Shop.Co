090486ed47987a148c13ee3991968bdc85f0bfda
Ibrahim <ibrahim.mneimneh0@gmail.com>
2024-11-15 00:44:35 +0100

    Fix cartModel

--- backend/src/models/orderModel.ts
+++ backend/src/models/orderModel.ts
@@ -1,15 +1,19 @@
 import mongoose, {Document, Schema} from "mongoose"
+import { IProductRef } from "../types/modalTypes";
 
 export interface IOrder extends Document {
     user:mongoose.Schema.Types.ObjectId,
-    products: mongoose.Schema.Types.ObjectId[],
+    products: IProductRef,
     totalPrice:Schema.Types.Decimal128,
     orderStatus:string
 }
 
 const userSchema = new Schema<IOrder>({
     user:{ type:mongoose.Schema.Types.ObjectId,ref:"User",required:true},
-    products:[{ type:mongoose.Schema.Types.ObjectId,ref:"product",required:true}],
+    products:[{productId:{type:mongoose.Schema.Types.ObjectId,ref:"Product",required:true},quantity:{type: Number, 
+      required: true,},
+    price:{ type: Schema.Types.Decimal128,  required: true }
+    }],
     totalPrice:{type:Schema.Types.Decimal128,required:true},
     orderStatus:{type:String,default:"Pending",enum:["Pending","In-delivery","Delivered"]}
 });
