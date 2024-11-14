8b9568af5a34c4826aefff18aef68a71c9283176
Ibrahim <ibrahim.mneimneh0@gmail.com>
2024-11-14 20:37:38 +0100

    Add modalTypes file

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,15 +3,13 @@ import dotenv from "dotenv"
 
 dotenv.config({ path: __dirname + '/.env' });
 const app = express();
+const PORT = process.env.PORT || 4004
 
 // Routes
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, TypeScript with Express!');
 });
 
-
-
-
-app.listen(process.env.PORT || 4004, () => {
-  console.log(`Server is running at http://localhost:${process.env.PORT?process.env.PORT:4004}`);
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
---
+++ backend/src/types/modalTypes.ts
@@ -0,0 +1,6 @@
+import mongoose, {Document, Schema} from "mongoose"
+export interface IProductRef {
+    productId: mongoose.Schema.Types.ObjectId;  
+    quantity: number;
+    price:Schema.Types.Decimal128     
+}
