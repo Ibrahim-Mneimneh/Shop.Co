63a9a7f0d9b7264f7250520e700adbdfdbeb7d66
Ibrahim <ibrahim.mneimneh0@gmail.com>
2024-11-15 14:42:10 +0100

    Update ModelTypes

--- backend/src/types/modalTypes.ts
+++ backend/src/types/modalTypes.ts
@@ -1,6 +1,9 @@
 import mongoose, {Document, Schema} from "mongoose"
+
 export interface IProductRef {
     productId: mongoose.Schema.Types.ObjectId;  
     quantity: number;
-    price:Schema.Types.Decimal128     
+    price?:Schema.Types.Decimal128
+    color:string,
+    size:string
 }
