22f5e498d995f7f63f374ea28dcb941e231415c6
Ibrahim <ibrahim.mneimneh0@gmail.com>
2024-11-15 20:13:32 +0100

    Add Decimal128-Number conversion

--- backend/src/types/modalTypes.ts
+++ backend/src/types/modalTypes.ts
@@ -1,7 +1,12 @@
 import mongoose, {Document, Schema} from "mongoose"
 
+export const decimal128ToNumber = (value: Schema.Types.Decimal128): number => parseFloat(value.toString());
+
+export const numberToDecimal128 = (value: number): mongoose.Types.Decimal128 =>
+  mongoose.Types.Decimal128.fromString(value.toFixed(2));
+
 export interface IProductRef {
-    productId: mongoose.Schema.Types.ObjectId;  
+    productId: mongoose.Schema.Types.ObjectId;
     quantity: number;
     price?:Schema.Types.Decimal128
     color:string,
