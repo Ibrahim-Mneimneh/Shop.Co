c24121cee53e56e3335bc1c66d287057c85d6203
Ibrahim <ibrahim.mneimneh0@gmail.com>
2024-12-09 22:59:49 +0100

    Remove decimal128 setters and getters

--- backend/src/types/modalTypes.ts
+++ backend/src/types/modalTypes.ts
@@ -3,13 +3,6 @@ import mongoose,{Types,Schema} from "mongoose"
 export type IObjectId=Types.ObjectId
 export type IDecimal=Types.Decimal128
 
-export const decimal128ToNumber = (value: Schema.Types.Decimal128): number => parseFloat(value.toString());
-
-
-export const numberToDecimal128 = (value: number): Types.Decimal128 =>
-  Types.Decimal128.fromString(value.toFixed(2));
-
-
 
 export interface IOrderQuantity{
   size:string,
