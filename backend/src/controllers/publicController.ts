48a566458b4c4e9c9903f38c5f86c7fb9fa71b7b
Ibrahim <ibrahim.mneimneh0@gmail.com>
2024-12-09 23:04:33 +0100

    Add getProduct controller

--- backend/src/controllers/publicController.ts
+++ backend/src/controllers/publicController.ts
@@ -1,7 +1,8 @@
 import { Request,RequestHandler,Response } from "express";
 import { IObjectId } from "../types/modalTypes";
-import mongoose from "mongoose";
+import mongoose, { Types } from "mongoose";
 import { ProductImageModel } from "../models/productImageModel";
+import { ProductModel } from "../models/productModel"
 
 export const getImage:RequestHandler = async (req:Request,res:Response)=>{
     try{
@@ -26,4 +27,31 @@ export const getImage:RequestHandler = async (req:Request,res:Response)=>{
         console.log(error)
         res.status(500).json({message:"Server Error"})
     }
+}
+
+// View products (with filters)
+
+// View product
+export const getProduct = async (req:Request,res:Response)=>{
+  try{
+        // get id prom params
+        const isValidProductId:boolean = Types.ObjectId.isValid(req.params.productId)
+        if(!isValidProductId){
+            res.status(404).json({message:"Invalid ProductId in URL"})
+            return
+        }
+        const productId:IObjectId= new mongoose.Types.ObjectId(req.params.productId)
+        // fetch for the product & variants
+        const variant = await ProductModel.getVariants(productId) 
+        if(variant.success && variant.productVariant && variant.product){
+            const product = variant.product.toJSON();
+            const productData = { ...product, variants: variant.productVariant };
+            res.status(200).json({message:"Product details sent successfully",data:productData})
+            return 
+        }
+        res.status(400).json({message:variant.errorMessage})  
+  }catch(error){
+        console.log("Get product -"+error)
+        res.status(500).json({message:"Server Error"})
+  }
 }
