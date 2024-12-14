c31945a6c414222e2019df9daabe05ed5bea277e
Ibrahim <ibrahim.mneimneh0@gmail.com>
2024-12-14 23:43:27 +0000

    Add UpdateStock type

--- backend/src/types/adminControllerTypes.ts
+++ backend/src/types/adminControllerTypes.ts
@@ -1,4 +1,6 @@
 import imageSize from "image-size";
+import { IObjectId } from "./modalTypes";
+import { IProduct } from "../models/productModel";
 
 export interface IBase64Image {
   type:string,
@@ -8,6 +10,19 @@ export interface IBase64Image {
 export interface IIsValidBase64 extends IBase64Image{
   success:boolean,
   base64ErrorMessage:string,
+}
+
+export interface IDetail {
+  size:string,
+  quantity:number,
+  success?:boolean,
+  message?:string
+}
+
+export interface IUpdateStock{
+  variant:IObjectId | string,
+  product?:IProduct,
+  details:IDetail[]
 }
 
 // Check size and validity of base64Images
