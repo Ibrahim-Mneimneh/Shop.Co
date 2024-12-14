0976b6aa108f356b2ed4e7d26448d15a173510f3
Ibrahim <ibrahim.mneimneh0@gmail.com>
2024-12-14 23:45:29 +0000

    Fix sessionMiddleware

--- backend/src/middleware/sessionMiddleware.ts
+++ backend/src/middleware/sessionMiddleware.ts
@@ -3,26 +3,29 @@ import mongoose, { ClientSession } from "mongoose";
 import { AuthRequest } from "./authMiddleware";
 
 export interface DbSessionRequest extends AuthRequest{
-    dbSession: ClientSession
+    dbSession?: ClientSession
 }
 
 export const sessionMiddleware = async (req:DbSessionRequest,res:Response,next:NextFunction)=>{ 
     const session = await mongoose.startSession();
     req.dbSession = session
+    let transactionEnded:boolean =false
     try{
         session.startTransaction()
         res.on('finish',async()=>{
-            if(res.statusCode>=200 && res.statusCode<300){
+            if(!transactionEnded && res.statusCode>=200 && res.statusCode<300){
                 await session.commitTransaction();
             }else{
                 await session.abortTransaction()
             }
+            await session.endSession()
         })
         next()
     }catch(error){
+        if (!transactionEnded) {
+            await session.abortTransaction(); // Ensure transaction is aborted on error
+            transactionEnded = true;
+        }
         res.status(500).json({message:"Server Error"})
-        await session.abortTransaction();
-    } finally{
-        session.endSession()
     }
 }
