60976c21f58e47294e2255ed822951acb3a4b01a
Ibrahim <ibrahim.mneimneh0@gmail.com>
2024-12-21 23:00:34 +0000

    Change attribute name for usability

--- backend/src/types/productTypes.ts
+++ backend/src/types/productTypes.ts
@@ -4,8 +4,7 @@ import Joi from "joi"
 import { IProduct } from "../models/product/productModel"
 import { IQuantity } from "../models/product/productVariantModel"
 import mongoose,{ Types } from "mongoose"
-import { helpers } from "handlebars"
-
+import { IObjectId } from "./modalTypes"
 
 export const validParamsIdSchema = Joi.string().pattern(/^[0-9a-fA-F]{24}$/).message('Invalid Id format').required().custom((value:string, helpers) => {
   if (!Types.ObjectId.isValid(value)) {
@@ -20,21 +19,6 @@ const quantitySchema =Joi.object<IQuantity
     quantityLeft:Joi.number().integer().min(0).required()
 })
 
-export const saleOptionsSchema = Joi.object({
-    startDate:Joi.date().iso().greater("now").required().messages({
-      "date.greater": "Sale start date must be in the future.",
-      "date.base": "Sale start date must be a valid date.",
-    }),
-    endDate:Joi.date().iso().greater(Joi.ref("startDate")).required().messages({
-      "date.greater": "Sale end date must be after the start date.",
-      "date.base": "Sale end date must be a valid date.",
-    }),
-    discountPercentage:Joi.number().min(1).max(99).required().messages({
-    "number.min": "Discount percentage must be at least 1%",
-    "number.max": "Discount percentage cannot exceed 99%",
-  })
-}).options({ convert: true }); 
-
 export const variantSchema=Joi.object({
     color:Joi.string().pattern(/^#([0-9a-fA-F]{3}){1,2}$/).message('Invalid hex color'),
     quantity: Joi.array().items(quantitySchema).min(1).required(),
@@ -48,12 +32,6 @@ export const variantSchema=Joi.object({
         })
       ).min(1).required(),
     originalPrice: Joi.number().min(0).required(),
-    isOnSale: Joi.boolean().default(false),
-    saleOptions: Joi.when("isOnSale", {
-      is: true,
-      then: saleOptionsSchema.required(),
-      otherwise: Joi.forbidden(),
-    }),
 })
 
 export const addProductSchema = Joi.object<IProduct>({
@@ -110,7 +88,7 @@ export const updateQuantitySchema =Joi.object({
 })
 
 export const deleteProductQuerySchema = Joi.object({
-  productId:validParamsIdSchema,
+  Id:validParamsIdSchema,
   clearStock: Joi.string().valid('true', 'false').optional().trim().lowercase().messages({
       "string.base": "Attribute clearStock must be a string ('true' or 'false').",
       "any.only": "Attribute clearStock can only be 'true' or 'false'.",
