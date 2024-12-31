fe02a9df2eb9f45b1a6302331be3070d40d336b3
Ibrahim <ibrahim.mneimneh0@gmail.com>
2024-12-31 22:09:27 +0100

    Add product search & filter route

--- backend/src/routes/publicRoutes.ts
+++ backend/src/routes/publicRoutes.ts
@@ -1,11 +1,11 @@
 
 import express, { Router } from 'express';
-import { getImage, getProduct } from '../controllers/publicController';
+import { getFilteredProducts, getImage, getProduct } from '../controllers/publicController';
 
 const router: Router = express.Router();
 
 router.get("/images/:imageId",getImage)
-
+router.get("/products/", getFilteredProducts)
 router.get("/products/:productId", getProduct)
 
 export const publicRouter=router
