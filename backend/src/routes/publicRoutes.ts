383076d9faa74ab87ed54e3d56a818a3aeb4c8c2
Ibrahim <ibrahim.mneimneh0@gmail.com>
2025-01-05 22:36:43 -0100

    Add getVariant route

--- backend/src/routes/publicRoutes.ts
+++ backend/src/routes/publicRoutes.ts
@@ -1,11 +1,61 @@
 
 import express, { Router } from 'express';
-import { getFilteredProducts, getImage, getProduct } from '../controllers/publicController';
+import { getFilteredProducts, getImage, getVariant} from '../controllers/publicController';
 
 const router: Router = express.Router();
 
 router.get("/images/:imageId",getImage)
 router.get("/products/", getFilteredProducts)
-router.get("/products/:productId", getProduct)
+router.get("/products/:variantId", getVariant)
 
 export const publicRouter=router
+
+/**
+ * @swagger
+ * tags:
+ *   - name: Public 
+ *     description: Operations related to Viewing, Searching or Filtering Products
+ */
+
+/**
+ * @swagger
+ * /products/:
+ *   post:
+ *     summary: Get filtered products  
+ *     tags:
+ *          - Public
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               firstname:
+ *                 type: string
+ *               lastname:
+ *                 type: string
+ *               country:
+ *                 type: string
+ *               postalCode:
+ *                 type: integer
+ *               bldngNum:
+ *                 type: integer
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *             example:
+ *               firstname: "John"
+ *               lastname: "Doe"
+ *               country: "USA"
+ *               postalCode: 12345
+ *               bldngNum: 101
+ *               email: "john.doe@example.com"
+ *               password: "TuenUI-88"
+ *     responses:
+ *       201:
+ *         description: User registered successfully
+ *       400:
+ *         description: Bad request
+ */
