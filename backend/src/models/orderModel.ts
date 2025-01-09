910a13aa3b143e13ccda65716fc2b59e7415a102
Ibrahim <ibrahim.mneimneh0@gmail.com>
2025-01-09 23:19:08 +0000

    Add expiresAt, payment & delivery status

--- backend/src/models/orderModel.ts
+++ backend/src/models/orderModel.ts
@@ -3,25 +3,52 @@ import mongoose, {Document,Types, Schema} from "mongoose"
 import { IObjectId, IProductRef } from "../types/modalTypes";
 
 export interface IOrder extends Document {
-    _id:Types.ObjectId,
-    user:IObjectId,
-    products: IProductRef,
-    totalPrice:number,
-    orderStatus:string
+  _id: Types.ObjectId;
+  user: IObjectId;
+  products: IProductRef;
+  totalPrice: number;
+  paymentStatus: "Pending" | "Complete";
+  deliveryStatus: "Pending"|"In-delivery"|"Delivered"
+  expiresAt?:Date
 }
 
-const orderSchema = new Schema<IOrder>({
-    user:{ type:mongoose.Schema.Types.ObjectId,ref:"User",required:true},
-    products:[{productId:{type:mongoose.Schema.Types.ObjectId,ref:"Product",required:true},quantity:{type: Number, required: true,},
-    price:{ type: Number, required: true,}
-    }],
-    totalPrice:{type:Number,required:true,},
-    orderStatus:{type:String,default:"Pending",enum:["Pending","In-delivery","Delivered"]}
-},{timestamps:true});
+const orderSchema = new Schema<IOrder>(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    products: [
+      {
+        productId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        quantity: { type: Number, required: true },
+        price: { type: Number, required: true },
+      },
+    ],
+    totalPrice: { type: Number, required: true },
+    deliveryStatus: {
+      type: String,
+      default: "Pending",
+      enum: ["Pending", "In-delivery", "Delivered"],
+    },
+    paymentStatus: {
+      type: String,
+      default: "Pending",
+      enum: ["Pending", "Complete"],
+    },
+    expiresAt:{type:Date}
+  },
+  { timestamps: true }
+);
 
 orderSchema.set("toJSON",{transform:(doc,ret)=>{
     delete ret.user
+    delete ret.expiresAt
+    delete ret.paymentStatus
     return ret
 }});
+
+orderSchema.index({ expiresAt: 1 }, { expireAfterSeconds: 0 });
 
 export const OrderModel =mongoose.model<IOrder>("Order",orderSchema);
