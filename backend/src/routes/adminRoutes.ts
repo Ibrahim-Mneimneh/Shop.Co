6eedf687e5b236d95b314e82d552765f5b190320
Ibrahim <ibrahim.mneimneh0@gmail.com>
2025-01-27 23:04:16 -0100

    Add getFeed route

--- backend/src/routes/adminRoutes.ts
+++ backend/src/routes/adminRoutes.ts
@@ -1,35 +1,64 @@
-import express, { Router } from 'express';
-import { addProduct, addProductImage, addProductVariant, adminLogin, deleteProduct, deleteProductVariant, deleteVariantSale, getProduct, restockProduct, updateDeliveryStatus, updateVariantSale } from '../controllers/adminController';
-import { adminAuthMiddleware, authMiddleware } from '../middleware/authMiddleware';
-import { sessionMiddleware } from '../middleware/sessionMiddleware';
+import express, { Router } from "express";
+import {
+  addProduct,
+  addProductImage,
+  addProductVariant,
+  adminLogin,
+  deleteProduct,
+  deleteProductVariant,
+  deleteVariantSale,
+  getDashboard,
+  getProduct,
+  restockProduct,
+  updateDeliveryStatus,
+  updateVariantSale,
+} from "../controllers/admin/adminController";
+import {
+  adminAuthMiddleware,
+  authMiddleware,
+} from "../middleware/authMiddleware";
+import { sessionMiddleware } from "../middleware/sessionMiddleware";
 
 const router: Router = express.Router();
 
 // Add product
-router.post("/login",adminLogin)
+router.post("/login", adminLogin);
 
 // Auth Middleware for routes
-router.use("/",adminAuthMiddleware)
-
-// Add Product 
-router.post("/products",addProduct)
+router.use("/", adminAuthMiddleware);
+// Get Dashboard 
+router.get("/dashboard",getDashboard);
+// Add Product
+router.post("/products", addProduct);
 // Add Product Images
-router.post("/products/images",addProductImage)
+router.post("/products/images", addProductImage);
 // Get Product & its variants
-router.get("/products/:productId",getProduct) // *********** Modify for later 
+router.get("/products/:productId", getProduct); // *********** Modify for later
 // Add varinat Sale or update it
-router.patch("/products/variants/sales/:variantId",sessionMiddleware,updateVariantSale)
+router.patch(
+  "/products/variants/sales/:variantId",
+  sessionMiddleware,
+  updateVariantSale
+);
 // Delete variant Sale
-router.delete("/products/variants/sales/:variantId",sessionMiddleware,deleteVariantSale)
+router.delete(
+  "/products/variants/sales/:variantId",
+  sessionMiddleware,
+  deleteVariantSale
+);
 // Soft Delete Product
-router.delete("/products/:productId",sessionMiddleware,deleteProduct)
+router.delete("/products/:productId", sessionMiddleware, deleteProduct);
 // Soft Delete Variant
-router.delete("/products/variants/:variantId",deleteProductVariant) 
+router.delete("/products/variants/:variantId", deleteProductVariant);
 // Restock Product
-router.patch("/products/restock/:productId",sessionMiddleware,restockProduct) 
+router.patch("/products/restock/:productId", sessionMiddleware, restockProduct);
 // Add Variant for a Product
-router.post("/products/:productId/variants",sessionMiddleware,addProductVariant) 
+router.post(
+  "/products/:productId/variants",
+  sessionMiddleware,
+  addProductVariant
+);
 // Update Order Status
 router.patch("/orders/:orderId/status", updateDeliveryStatus);
 
-export const adminRoutes=router
+export const adminRoutes = router;
