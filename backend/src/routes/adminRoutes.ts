1c0f75ce9c714da56ad6f8135c2513b9c4f00bc4
Ibrahim <ibrahim.mneimneh0@gmail.com>
2025-02-09 23:19:15 +0100

    Add reActivateProduct route

--- backend/src/routes/adminRoutes.ts
+++ backend/src/routes/adminRoutes.ts
@@ -1,69 +1,44 @@
 import express, { Router } from "express";
 import {
-  addProduct,
   addProductImage,
-  addProductVariant,
   adminLogin,
-  deleteProduct,
-  deleteProductVariant,
-  deleteVariantSale,
   getProduct,
-  restockProduct,
   updateDeliveryStatus,
-  updateVariantSale,
 } from "../controllers/admin/adminController";
 import {
   adminAuthMiddleware,
-  authMiddleware,
 } from "../middleware/authMiddleware";
 import { sessionMiddleware } from "../middleware/sessionMiddleware";
 import { getDashboard, getMostSoldProducts, getPendingOrders, getRecentOrders } from "../controllers/admin/dashboard/dashboard";
+import { addProduct, addProductVariant, deleteProduct, deleteProductVariant, reActivateProduct, restockProduct } from "../controllers/admin/product/productController";
+import { deleteVariantSale, updateVariantSale } from "../controllers/admin/product/saleController";
 
 const router: Router = express.Router();
 
-// Add product
+// Admin Login
 router.post("/login", adminLogin);
 
-// Auth Middleware for routes
-router.use("/", adminAuthMiddleware);
-// Get Dashboard 
-router.get("/dashboard",getDashboard);
-// Add Product
+// Authentication Middleware for all subsequent routes
+router.use(adminAuthMiddleware);
+
+// Dashboard routes
+router.get("/dashboard", getDashboard);
+router.get("/orders/pending", getPendingOrders);
+router.get("/orders/recent", getRecentOrders);
+router.get("/products/mostSold", getMostSoldProducts);
+
+// Product routes
 router.post("/products", addProduct);
-// Add Product Images
+router.get("/products/:productId", getProduct); // Modify for later
 router.post("/products/images", addProductImage);
-// Get most Sold Products 
-router.get("/products/mostSold",getMostSoldProducts)
-// Get delivery pending Products 
-router.get("/orders/pending",getPendingOrders)
-// Get most Sold Products 
-router.get("/orders/recent",getRecentOrders)
-// Get Product & its variants
-router.get("/products/:productId", getProduct); // *********** Modify for later
-// Add varinat Sale or update it
-router.patch(
-  "/products/variants/sales/:variantId",
-  sessionMiddleware,
-  updateVariantSale
-);
-// Delete variant Sale
-router.delete(
-  "/products/variants/sales/:variantId",
-  sessionMiddleware,
-  deleteVariantSale
-);
-// Soft Delete Product
+router.post("/products/:productId/variants", sessionMiddleware, addProductVariant);
+router.patch("/products/variants/sales/:variantId", sessionMiddleware, updateVariantSale);
+router.delete("/products/variants/sales/:variantId", sessionMiddleware, deleteVariantSale);
 router.delete("/products/:productId", sessionMiddleware, deleteProduct);
-// Soft Delete Variant
+router.patch("/products/:productId/activate",sessionMiddleware,reActivateProduct);
 router.delete("/products/variants/:variantId", deleteProductVariant);
-// Restock Product
 router.patch("/products/restock/:productId", sessionMiddleware, restockProduct);
-// Add Variant for a Product
-router.post(
-  "/products/:productId/variants",
-  sessionMiddleware,
-  addProductVariant
-);
+
 // Update Order Status
 router.patch("/orders/:orderId/status", updateDeliveryStatus);
 
