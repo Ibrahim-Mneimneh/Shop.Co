ae79b138516ce5696bd3e41820ba4b801401a450
Ibrahim <ibrahim.mneimneh0@gmail.com>
2025-02-19 00:41:26 +0100

    Fix productSearch controller

--- backend/src/controllers/admin/search/searchController.ts
+++ backend/src/controllers/admin/search/searchController.ts
@@ -1,19 +1,26 @@
 import { Response } from "express";
 import { AuthRequest } from "../../../middleware/authMiddleware";
-import { paginationSchema } from "../../../types/adminControllerTypes";
-import { searchOrderAgg } from "./aggregates";
-import { getLowOnStockAgg } from "../dashboard/aggregates";
+import { adminFilterProductsSchema } from "../../../types/adminControllerTypes";
+import { searchOrderAgg, searchProductAgg } from "./aggregates";
 
 // Write search orders
 export const orderSearch = async (req: AuthRequest, res: Response) => {
-  const { error, value } = paginationSchema.validate(req.query);
-  if (error) {
-    res.status(400).json({
-      message:
-        "Validation failed: " + error.details[0].message.replace(/\"/g, ""),
-    });
-    return;
-  }
+  const query = req.query;
+      const sizeQuery = req.query.size;
+      req.query.size =
+        sizeQuery && typeof sizeQuery === "string"
+          ? sizeQuery.split(",")
+          : undefined;
+      const { value, error } = adminFilterProductsSchema.validate(query, {
+        stripUnknown: true,
+      });
+      if (error) {
+        res.status(400).json({
+          message:
+            "Validation failed: " + error.details[0].message.replace(/\"/g, ""),
+        });
+        return;
+      }
   const { page = 1, limit = 10 } = value;
   const skip = (page - 1) * limit;
   try {
@@ -50,7 +57,15 @@ export const orderSearch = async (req: AuthRequest, res: Response) => {
 
 // Write product search
 export const productSearch = async (req: AuthRequest, res: Response) => {
-  const { error, value } = paginationSchema.validate(req.query);
+  const query = req.query;
+  const sizeQuery = req.query.size;
+  req.query.size =
+    sizeQuery && typeof sizeQuery === "string"
+      ? sizeQuery.split(",")
+      : undefined;
+  const { value, error } = adminFilterProductsSchema.validate(query, {
+    stripUnknown: true,
+  });
   if (error) {
     res.status(400).json({
       message:
@@ -61,16 +76,13 @@ export const productSearch = async (req: AuthRequest, res: Response) => {
   const { page = 1, limit = 10 } = value;
   const skip = (page - 1) * limit;
   try {
-    const { result, totalCount: count } = await getLowOnStockAgg(
-      value,skip
-    );
+    const { result, totalCount:count } = await searchProductAgg(value,skip);
     if (!result || result.length === 0) {
       res.status(404).json({ message: "No matching products found" });
       return;
     }
-    const totalCount = count[0].count;
-    const totalPages: number =
-      totalCount <= limit ? 1 : Math.ceil(totalCount / limit);
+    const totalCount = count.count;
+    const totalPages: number =totalCount <= limit ? 1 : Math.ceil(totalCount / limit);
     if (page > totalPages) {
       res.status(400).json({
         message:
@@ -83,7 +95,7 @@ export const productSearch = async (req: AuthRequest, res: Response) => {
       data: {
         totalPages,
         currentPage: page,
-        orders: result,
+        products: result,
       },
     });
   } catch (error) {
