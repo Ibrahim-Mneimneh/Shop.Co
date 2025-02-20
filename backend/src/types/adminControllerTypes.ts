dd0d8abea7def0d324f82d83d7bc669aa1338b29
Ibrahim <ibrahim.mneimneh0@gmail.com>
2025-02-20 23:04:25 +0000

    Add orderSearchSchema

--- backend/src/types/adminControllerTypes.ts
+++ backend/src/types/adminControllerTypes.ts
@@ -15,13 +15,8 @@ export const getDashboardSchema = Joi.object({
 });
 
 export const paginationSchema = Joi.object({
-  page: Joi.number()
-    .integer()
-    .min(0)
-    .required(),
-  limit: Joi.number()
-    .valid(5, 10)
-    .optional().default(10)
+  page: Joi.number().integer().min(0).required(),
+  limit: Joi.number().valid(5, 10).optional().default(10),
 });
 
 // Extend paginationSchema
@@ -32,7 +27,7 @@ export const getMostSoldProductsSchema = paginationSchema.keys({
 // Extend filterProductSchema
 export const adminFilterProductsSchema = filterProductsSchema.keys({
   unitsSoldRange: Joi.string()
-    .valid("0-50", "0-100", "0-500", "500-1000","1000-10000", "10000")
+    .valid("0-50", "0-100", "0-500", "500-1000", "1000-10000", "10000")
     .optional(),
   status: Joi.string().valid("Active", "Inactive").optional(),
   minCost: Joi.number().min(0).max(99999).default(0),
@@ -40,5 +35,34 @@ export const adminFilterProductsSchema = filterProductsSchema.keys({
   inStock: Joi.string().valid("InStock", "OutofStock", "LowStock"), // override
   quantityLeft: Joi.string()
     .valid("0-50", "50-100", "100-200", "200-300", "300-400", "400-500", "+500")
+    .optional(),
+});
+
+export const adminFilterOrdersSchema = Joi.object({
+  orderedAt: Joi.date()
+    .iso()
+    .less("now")
+    .optional()
+    .custom((value, helpers) => {
+      // Get format YYYY-MM-DD
+      const orderedAtDate = new Date(value);
+      return orderedAtDate.toISOString().split("T")[0];
+    })
+    .messages({
+      "date.greater": "Ordered at date must be in the past.",
+      "date.base": "Ordered at date must be a valid date.",
+    }),
+  deliveryStatus: Joi.string()
+    .valid("Pending", "Complete", "Failed")
+    .optional(),
+  minProfit: Joi.number().min(0).max(99999).default(0),
+  maxProfit: Joi.number().min(Joi.ref("minProfit")).max(99999),
+  minPrice: Joi.number().min(0).max(99999).default(0),
+  maxPrice: Joi.number().min(Joi.ref("minPrice")).max(99999),
+  country: Joi.string()
+    .regex(/^[a-zA-Z\s]+$/)
+    .optional(),
+  name: Joi.string()
+    .regex(/^[a-zA-Z\s]+$/)
     .optional(),
 });
